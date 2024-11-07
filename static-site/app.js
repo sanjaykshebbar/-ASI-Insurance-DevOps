8219b67c317d58279750a100832e19f0392c46a8
Sanjay KS <98266129+sanjaykshebbar@users.noreply.github.com>
2024-11-07 22:20:01 +0530

    Create app.js

---
+++ static-site/app.js
@@ -0,0 +1,15 @@
+  GNU nano 6.2                                 app.js                                           
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
